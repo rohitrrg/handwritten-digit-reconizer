7cc4511210ba527704f76b3689086ece7f756c3c
rohitrrg <rohitgadhwar05@gmail.com>
2022-03-15 20:22:38 +0530

    update

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ import numpy as np
 import base64
 import matplotlib.pyplot as plt
 import pickle
+import keras
 
 init_Base64 = 21
 
 with open('model_pkl', 'rb') as f:
-    model = pickle.load(f)
+    ml_model = pickle.load(f)
+
+ann_model = keras.models.load_model('my_keras_model.h5')
 
 app = Flask(__name__)
 
@@ -24,16 +27,26 @@ def predict():
         draw = request.form['url']
         draw = draw[init_Base64:]
         draw_decoded = base64.b64decode(draw)
-        image = np.asarray(bytearray(draw_decoded), dtype="uint8")
+        image = np.asarray(bytearray(draw_decoded))
         image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
         resized = cv2.resize(image, (28,28), interpolation = cv2.INTER_AREA)
+        print("resised shape:", resized.shape)
         vect = np.asarray(resized, dtype="uint8")
         plt.imshow(vect, cmap='gray')
         plt.savefig('static/plot.png')
         vect = vect.reshape(1,784)
-        result = model.predict(vect)
+        ml_result = ml_model.predict(vect)
+        
+        # Neural Networks
+        img = (np.expand_dims(resized, 0))
+        ann_result = ann_model.predict(img/255.0)
+        
+        
 
-    return render_template('index.html', name='new_plot', url ='/static/plot.png', result = result[0])
+    return render_template('index.html', name='new_plot', url ='/static/plot.png',
+                           ann_result = np.argmax(ann_result, axis=1)[0], 
+                           ann_probs=ann_result.round(2), 
+                           ml_result=ml_result[0])
 
 if __name__=="__main__":
     app.run(port="8000", debug=True)
